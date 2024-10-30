8d938a500ab00b9a96239c8d0798ef92afd7c696
Viktor Jensen-Torp <v.jensentorp@gmail.com>
2024-10-30 17:47:33 +0100

    chore: fix bundleanalyzer

--- apps/invest/next.config.js
+++ apps/invest/next.config.js
@@ -3,6 +3,7 @@ import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const bundleAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
+  openAnalyzer: false,
 });
 
 const baseConfig = createNextConfig({
