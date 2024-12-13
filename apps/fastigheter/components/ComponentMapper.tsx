c2648286c05511b5ec6bdd231740dfe0a39bbe70
Viktor Jensen-Torp <v.jensentorp@gmail.com>
2024-12-13 18:03:26 +0100

    meaningless change

--- apps/fastigheter/components/ComponentMapper.tsx
+++ apps/fastigheter/components/ComponentMapper.tsx
@@ -64,7 +64,7 @@ export async function ComponentMapper(data: ComponentMapperProps) {
           return null;
         }
 
-        // @todos type here is not good
+        // @todos type here is not good!
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         return <Component key={`${props.id}-${i}`} {...(props as any)} />;
       })}
